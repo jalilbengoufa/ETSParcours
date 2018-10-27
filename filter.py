3f2a7913b763a91e9b0df5737ba0390e04d696f9
jalilbengoufa <jalilbengoufa@gmail.com>
2018-10-27 02:42:46 -0400

    second commit

--- filter.py
+++ filter.py
@@ -11,21 +11,22 @@ os.system('sed 1d '+localFilesPath+'*MEC.csv > '+localFilesPathMerged+'MEC.csv')
 os.system('sed 1d '+localFilesPath+'*GPA.csv > '+localFilesPathMerged+'GPA.csv')
 os.system('sed 1d '+localFilesPath+'*SEG.csv > '+localFilesPathMerged+'SEG.csv')
 
-
+header = ['sigle', 'name', 'session1','session2','session3','session4','session5']
 ## filter cs files by removing unwanted charters and lines
 for filename in os.listdir(localFilesPathMerged):
     if filename.endswith(".csv"):
         with open(localFilesPathMerged+filename, 'r') as f:
             data = list(csv.reader(f))
-
+        ##filter sigle 
         import collections
         counter = collections.defaultdict(int)
         for row in data:
             row[0] = row[0][-6:]
             
+        ## filtre uneeded lines and characters
         writer = csv.writer(open(localFilesPathMerged+filename, 'w'))
+        writer.writerow(header)  
         for rowWrite in data:
             if (rowWrite[0] != 'COURS' and rowWrite[0][0] != 'T' and rowWrite[0][0] != 'S' and rowWrite[0][0] != 'J'  
                 and len(rowWrite) == 7 and  rowWrite[1] != '' ):
                 writer.writerow(rowWrite)           
-   
